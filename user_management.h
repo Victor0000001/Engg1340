d58501436eb6a1bdd54896d9dfecd0f998f4be61
KatherineHuang77 <u3639247@connect.hku.hk>
2025-04-30 17:51:46 +0800

    Update user_management.h

--- user_management.h
+++ user_management.h
@@ -5,29 +5,29 @@
 #include <vector>
 using namespace std;
 
-// 玩家结构
+// Player structure
 struct Player {
-    string username; // 用户名
-    string password; // 密码
-    int atk; // 攻击力
-    int hp; // 当前生命值
-    int max_hp; // 最大生命值
-    int currency; // 宇宙货币
-    int storage; // 存储空间
-    string difficulty; // 难度级别
-    vector<string> inventory; // 背包
-    vector<string> equipment; // 装备
-    vector<string> energies; // 收集的能源
-    bool autoLogin; // 注册后自动登录
+    string username; // username
+    string password; // password
+    int atk; // attack damage
+    int hp; // current hp
+    int max_hp; // max hp
+    int currency; // cosmic currency
+    int storage; // storage space
+    string difficulty; // difficulty level
+    vector<string> inventory; // backpack
+    vector<string> equipment; // equipment
+    vector<string> energies; // collected energy
+    bool autoLogin; // automatically log in after registration
     bool isGameComplete;
 };
 
-// 函数声明
-void initializeUser(Player& player, string username, string password, string difficulty);//初始化玩家
-void registerUser(Player& player);//注册
-bool login(Player& player);//登录
-void saveGame(const Player& player);//保存数据
-void loadGame(Player& player);//加载数据
-bool usernameExists(const string& username);//判断用户名是否存在
+// function declaration
+void initializeUser(Player& player, string username, string password, string difficulty); //initialize player
+void registerUser(Player& player);//registration
+bool login(Player& player);//log in
+void saveGame(const Player& player);//save data
+void loadGame(Player& player);//loading data
+bool usernameExists(const string& username);//Determine if the username exists
 
 #endif
